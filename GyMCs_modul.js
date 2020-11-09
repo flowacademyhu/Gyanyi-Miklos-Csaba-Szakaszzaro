e552a522da21868697547d9076ac206d2cf7f7a0
gyanyim <gyanyi.miklos@gmail.com>
2020-11-09 12:02:30 +0100

    A ProgAlap vizsga modulja

---
+++ GyMCs_modul.js
@@ -0,0 +1,17 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const multip = (a, b) => {
+    return a * b;
+}
+
+const pow = (a,b) => {
+    return a**b;
+}
+
+module.exports = {
+    add,
+    multip,
+    pow,
+}
